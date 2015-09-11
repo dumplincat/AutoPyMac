19997f431da767aff2ce6e9b270ece6bef0a9b35
zbo <zbo2@qq.com>
2015-09-12 06:33:33 +0800

    add auto.py

--- auto.py
+++ auto.py
@@ -1,2 +1,8 @@
 import atomac
+
 atomac.launchAppByBundleId('us.zoom.ringcentral')
+automator = atomac.getAppRefByBundleId('us.zoom.ringcentral')
+window = automator.windows()[0]
+print window.AXTitle
+all=window.findAll()
+print all
